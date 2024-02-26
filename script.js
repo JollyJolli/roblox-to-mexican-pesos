d1bd00091533775fa9cc3b0caa76f9278d3f3be5
Jolly <83844571+JollyJolli@users.noreply.github.com>
2024-02-26 15:42:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function calcularRobux() {
     var pesos = document.getElementById("pesos").value;
-    var robux = pesos / 3.10077519;
+    var robux = pesos / 3.1;
     document.getElementById("resultado").innerHTML = pesos + " pesos son aproximadamente <strong>" + robux.toFixed(2) + "</strong> Robux.";
 }
 
 function calcularPesos() {
     var robux = document.getElementById("robux").value;
-    var pesos = robux * 3.10077519;
+    var pesos = robux * 3.1;
     document.getElementById("resultado").innerHTML = robux + " Robux son aproximadamente <strong>" + pesos.toFixed(2) + "</strong> pesos.";
 }
